9bba8b69b849a4b793018c002d173120278222b6
Mehedi Hasan Rabbi <mehedi.saiyan@gmail.com>
2026-01-27 17:52:11 +0600

    static added

--- starlightpath/settings.py
+++ starlightpath/settings.py
@@ -189,11 +189,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = os.getenv("STATIC_URL", "static/")
-
-STATICFILES_DIRS = [BASE_DIR / "static"]
-
 STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 MEDIA_URL = os.getenv("MEDIA_URL", "/media/")
 MEDIA_ROOT = Path(os.getenv("MEDIA_ROOT", str(BASE_DIR / "media")))
